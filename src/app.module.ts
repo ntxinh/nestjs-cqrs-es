639df9a41fd26c97e1f0dfdf41bd7f74fc8254dc
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2019-12-04 15:31:45 +0700

    :sparkles: Update typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,7 @@ import { HeroesGameModule } from './heroes/heroes.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(
-      {
-        type: 'mssql',
-        host: 'localhost',
-        port: 1433,
-        username: 'sa',
-        password: '123',
-        database: 'boilerplate_development',
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true
-      }
-    ),
+    TypeOrmModule.forRoot(),
     HeroesGameModule
   ],
 })
