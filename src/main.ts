633ec5ca6c83e3d4682a5941d180e6e25103012b
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2019-12-09 19:45:08 +0700

    :sparkles: Add API version

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ async function bootstrap(): Promise<any> {
     .setTitle(config.TITLE)
     .setDescription(config.DESCRIPTION)
     .setVersion(config.VERSION)
-    // .setBasePath(`/${config.PREFIX}`)
+    .addServer(`/${config.PREFIX}`)
     .build();
   const document = SwaggerModule.createDocument(app, documentOptions);
-  // app.setGlobalPrefix(config.PREFIX);
+  app.setGlobalPrefix(config.PREFIX);
   app.enableCors();
   SwaggerModule.setup(config.API_EXPLORER_PATH, app, document);
   await app.listen(config.PORT, () =>
